e03a7e8de8e4022f9ef6b777d1c48dbe98d11f66
Diego <dpardoblacutt@gmail.com>
2022-09-20 18:55:20 -0400

    Refactor addBooksToTable() function into smaller functions

--- script.js
+++ script.js
@@ -34,65 +34,107 @@ function displayBooks(){
 function addBooksToTable(){
     const tbody = document.querySelector(".tbody");
     myLibrary.forEach( (book, index) => {
-        let tr = document.createElement("tr");
-        let th = document.createElement("th");
-        th.setAttribute("scope","row");
-        th.textContent = index +1;
-
-        let title = document.createElement("td");
-        title.textContent = book.title;
-        let author = document.createElement("td");
-        author.textContent = book.author;
-        let pagesNumber = document.createElement("td");
-        pagesNumber.textContent = book.pagesNumber;
-        let readTheBook = document.createElement("td");
-        readTheBook.textContent = book.readTheBook;
-
-        let td = document.createElement("td");
-        let changeStatusBtn = document.createElement("button");
-        changeStatusBtn.classList.add("btn", "btn-warning");
-        changeStatusBtn.textContent = "Change Status";
-        let deleteBtn = document.createElement("button");
-        deleteBtn.classList.add("btn", "btn-danger");
-        deleteBtn.textContent = "Remove Book";
-        td.append(changeStatusBtn,deleteBtn);
-        tr.append(th,title,author,pagesNumber,readTheBook, td);
+        let tr = createNewRow(index,book);
         tbody.append(tr);
     });
 }
 
+function createNewRow(index,book){
+  let tr = document.createElement("tr");
+  let th = createHeader(index);
+  let rowData = createRowData(book);
+  let td = document.createElement("td");
+  let buttons = createButtons(index);
+  td.append(buttons.changeStatusBtn,buttons.deleteBtn);
+  tr.append(th,rowData.title,rowData.author,rowData.pagesNumber,rowData.readTheBook, td);
+  return tr;
+}
+
+function createHeader(index){
+  let th = document.createElement("th");
+  th.setAttribute("scope","row");
+  th.textContent = index +1;
+  return th;
+}
+
+function createRowData(book){
+  let title = document.createElement("td");
+  title.textContent = book.title;
+  let author = document.createElement("td");
+  author.textContent = book.author;
+  let pagesNumber = document.createElement("td");
+  pagesNumber.textContent = book.pagesNumber;
+  let readTheBook = document.createElement("td");
+  readTheBook.textContent = book.readTheBook;
+  return {title,author,pagesNumber,readTheBook}
+}
+
+function createButtons(index){
+  let changeStatusBtn = createChangeStatusButton(index);
+  let deleteBtn = createDeleteButton(index)
+  return {changeStatusBtn,deleteBtn};
+}
+
+function createChangeStatusButton(index){
+  let changeStatusBtn = document.createElement("button");
+  changeStatusBtn.classList.add("btn", "btn-warning");
+  changeStatusBtn.textContent = "Change Status";
+  changeStatusBtn.onclick = function(){
+    if(myLibrary[index].readTheBook === "not read yet"){
+      myLibrary[index].readTheBook = "readed";
+      refreshTable()
+    }else if (myLibrary[index].readTheBook === "readed"){
+      myLibrary[index].readTheBook = "not read yet";
+      refreshTable()
+    }
+  }
+  return changeStatusBtn;
+}
+
+function createDeleteButton(index){
+  let deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("btn", "btn-danger");
+  deleteBtn.textContent = "Remove Book";
+  deleteBtn.onclick = function(){
+    myLibrary.splice(index,1);
+    refreshTable();
+  }
+  return deleteBtn;
+}
+
+
 function createModal(){
 
-var modal = document.getElementById("myModal");
+  var modal = document.getElementById("myModal");
 
-var btn = document.getElementById("addBtn");
+  var btn = document.getElementById("addBtn");
 
-var closeBtn = document.getElementById("closeBtn");
+  var closeBtn = document.getElementById("closeBtn");
 
-var span = document.getElementsByClassName("close")[0];
+  var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-closeBtn.onclick = function() {
-  if(!(this.form.title.value==="" || this.form.author.value==="" || this.form.pagesNumber.value==="" || this.form.readTheBook.value==="")){
-    modal.style.display = "none";
-    addBookToArray(this.form);
-    emptyForm(this.form);
-  }else{
-    alert("Please fill all the fields");
+  btn.onclick = function() {
+    modal.style.display = "block";
   }
-}
 
-span.onclick = function() {
-  modal.style.display = "none";
-}
-window.onclick = function(event) {
-  if (event.target == modal) {
+  closeBtn.onclick = function() {
+    if(!(this.form.title.value==="" || this.form.author.value==="" || this.form.pagesNumber.value==="" || this.form.readTheBook.value==="")){
+      modal.style.display = "none";
+      addBookToArray(this.form);
+      emptyForm(this.form);
+    }else{
+      alert("Please fill all the fields");
+    }
+  }
+
+  span.onclick = function() {
     modal.style.display = "none";
   }
-}
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 
 }
 
@@ -114,6 +156,7 @@ function refreshTable(){
   emptyTable();
   addBooksToTable();
 }
+
 
 function addBookToArray(form) {
   let newBook = new Book(form.title.value, form.author.value, form.pagesNumber.value, form.readTheBook.value);
